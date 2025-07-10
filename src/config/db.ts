6a3a76cc9202cc6b885ea490e4ad9d8f5390da12
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-07-09 22:09:49 -0700

    colors added

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
-
+import color from 'colors'
 export const connectDB = async () => {
     try{
         const {connection} = await mongoose.connect(process.env.MONGUITO)
         const url = `${connection.host}:${connection.port}`
 
-        console.log(`connection established: ${url}`);
+        console.log(color.inverse.green(`\tConnected to MongoDB\t ${url}`));
     }catch(e){
         // console.log(e);
         console.log(`Error connecting to DB: ${e.message}`);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import server from "./server";
+import color from "colors";
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, ()=>{
-    console.log("---------------------------------------");
-    console.log(`Server is running on port:\t ${PORT}\nLocal link: \thttp://localhost:${PORT}/`);
-    console.log("---------------------------------------");
+    console.log(color.red("---------------------------------------"));
+    console.log(color.inverse.bold.blue(`Server is running on port: ${PORT}\ndefault web link: \n\thttp://localhost:${PORT}/`));
+    console.log(color.red("---------------------------------------"));
 
 
 });
