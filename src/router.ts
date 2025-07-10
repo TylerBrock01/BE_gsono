d669668f17d0db898f2e45a29c56e5abc97cff21
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-07-10 12:00:40 -0700

    instance bussined added

--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,9 @@ const router = Router()
 
 router.post('/auth/register', async (req, res) => {
     console.log('register:\n',req.body);
-    await Bussines.create(req.body)
-    res.send('register successful: '+ req.body);
+    const bussines = new Bussines(req.body);
+
+    await bussines.save();
+    res.send('register successful: ');
 })
 export default router;
