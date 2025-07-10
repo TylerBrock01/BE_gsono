dc1628deeddc0fd380a16c53cfef0d5e379c097c
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-07-10 12:20:21 -0700

    to string added

--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,6 @@ router.post('/auth/register', async (req, res) => {
     const bussines = new Bussines(req.body);
 
     await bussines.save();
-    res.send('register successful: ');
+    res.send('register successfully\n'+bussines.toString());
 })
 export default router;
