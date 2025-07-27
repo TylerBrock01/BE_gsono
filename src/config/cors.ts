6e08dd4f49c03ed60edcff9c555b6023cf9e00d9
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-07-27 01:16:31 -0700

    env variable added

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,12 +1,10 @@
 import { CorsOptions } from 'cors'
 export const corsconfig: CorsOptions = {
     origin: function(origin, callback){
-        // callback(null, true)
         console.log(origin)
         if(origin ===process.env.FRONTEND_BUSSINES){
             callback(null,true)
             console.log('Allowed by CORS')
-            // return true;
         }else {
             // callback(new Error('Not allowed by CORS'))
             console.log('Not allowed by CORS')
