89759db3dde99b0c3340b947459de92258bb4df2
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-07-28 23:15:58 -0700

    show bussines

---
+++ src/handlers/ShowChainBussines.ts
@@ -0,0 +1,19 @@
+import Bussines from "../models/Bussines";
+import { Request, Response } from "express";
+
+export const showchainBussines = async (req:Request,res:Response) => {
+    const {chain} = req.body;
+    const bussinesExist = await Bussines.find({chain})
+
+    if(bussinesExist.length !== 0){
+        console.log('exist!')
+        // const bussines = bussinesExist.map(doc => new Business(doc.toObject()));
+        res.status(302).json(bussinesExist)
+        return console.log('closed')
+    }
+    else{
+        const error = new Error('Bussines no exist')
+        res.status(404).json({error: error.message})
+        return console.log('closed')
+    }
+};
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,13 @@
 import {Router} from 'express'
 import {createBussiness} from "./handlers/CreateBussines";
 import {selectBussines} from "./handlers/SelectBussines";
+import {showchainBussines} from "./handlers/ShowChainBussines";
 
 const router = Router()
 
 router.post('/auth/registerbussines', createBussiness)
 router.get('/showbussines', selectBussines)
+router.get('/showchainbussines', showchainBussines)
 
 
 
